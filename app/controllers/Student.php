0d8be65e89a2832038ab5cff8f7ef966854fc8f0
Marcos Matsuda <marcos.matsuda@MacBook-Pro-de-Marcos.local>
2018-07-08 01:07:36 -0300

    completing crud

--- app/controllers/Student.php
+++ app/controllers/Student.php
@@ -22,76 +22,127 @@ class Student{
             $statement = $this->container->db->prepare("SELECT * FROM students");
             $statement->execute();
             $results = $statement->fetchAll(\PDO::FETCH_ASSOC);
-            return json_encode($results);
+
+            $return = $response->withJson($results, 200)
+                ->withHeader('Content-type', 'application/json');
 
         }
-        catch(PDOException $e)
+        catch(\Exception $e)
         {
-            print $e->getMessage();
+            $return = $response->withJson("Operação não realizada.", 400)
+                ->withHeader('Content-type', 'application/json');
         }
+
+        return $return;
 
     }
 
     public function create( Request $request, Response $response )
     {
+        try{
 
+            $params = $request->getParsedBody();
+
+            if( is_null($params['name']) || is_null($params['gender']) || is_null($params['age'])  ){
+                return $response->withJson("Operação não realizada.", 400)
+                    ->withHeader('Content-type', 'application/json');
+            }
+
+            if( !is_numeric($params['age']) ){
+                return $response->withJson("Operação não realizada.", 400)
+                    ->withHeader('Content-type', 'application/json');
+            }
+
+            $sql = "INSERT INTO students (fullname, gender, age ) VALUES ( :fullname, :gender, :age);";
+
+            $statement = $this->container->db->prepare($sql);
+            $statement->bindParam(":fullname", $params['name'], \PDO::PARAM_STR);
+            $statement->bindParam(":gender", $params['gender'], \PDO::PARAM_STR);
+            $statement->bindParam(":age", $params['age'], \PDO::PARAM_INT);
+            $statement->execute();
+
+            $return = $response->withJson("Operação realizada com sucesso.", 200)
+                ->withHeader('Content-type', 'application/json');
+
+        }catch( \Exception $e ){
+
+            $return = $response->withJson("Operação não realizada.", 400)
+                ->withHeader('Content-type', 'application/json');
+        }
+        
+        return $return;
     }
 
     public function show( Request $request, Response $response, $args )
     {
-        $id = $args['id'];
-
         $sql = "SELECT * FROM students WHERE id = :id";
         $sth = $this->container->db->prepare($sql);
         $sth->bindParam(':id', $args['id'], \PDO::PARAM_INT);
         $sth->execute();
 
         $results = $sth->fetchAll(\PDO::FETCH_ASSOC);
-        $json = json_encode($results);
-        $return = $response->withJson([$results], 200)
+
+        $return = $response->withJson($results, 200)
             ->withHeader('Content-type', 'application/json');
 
         return $return;
     }
 
-    public function update( Request $request, Response $response, $args )
+
+    /**
+     *
+     * Headers:
+     * Content-Type: application/json
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return mixed
+     */
+    public function update(Request $request, Response $response, $args )
     {
-        $id = $args['id'];
-        $name = $request->getParam('name');
-        $age = $request->getParam('age');
-        $gender = $request->getParam('gender');
+        try{
 
-        var_dump($name);
+            $params = $request->getParsedBody();
 
+            $sql = "UPDATE students SET fullname = :fullname, age = :age, gender = :gender WHERE id = :id";
+            $statement = $this->container->db->prepare($sql);
 
-        die('update');
+            $statement->bindParam(":fullname", $params['name'], \PDO::PARAM_STR);
+            $statement->bindParam(":age",$params['age'], \PDO::PARAM_STR);
+            $statement->bindParam(":gender", $params['gender'], \PDO::PARAM_STR);
+            $statement->bindParam(":id", $args['id'], \PDO::PARAM_INT);
+            $statement->execute();
 
-        $sql = "UPDATE students SET fullname = :fullname, age = :age, gender = :gender WHERE id = :id";
-        $statement = $this->container->db->prepare($sql);
+            $return = $response->withJson("Operação realizada com sucesso.", 200)
+                ->withHeader('Content-type', 'application/json');
 
-        $statement->bindParam(":fullname", $name, \PDO::PARAM_STR);
-        $statement->bindParam(":age",$age, \PDO::PARAM_STR);
-        $statement->bindParam(":gender", $gender, \PDO::PARAM_STR);
-        $statement->bindParam(":id", $id, \PDO::PARAM_INT);
-        $statement->execute();
+        }catch (\Exception $e){
 
-        $return = $response->withJson(['msg' => "Operação realizada com sucesso."], 400)
-            ->withHeader('Content-type', 'application/json');
+            $return = $response->withJson("Operação não realizada.", 400)
+                ->withHeader('Content-type', 'application/json');
+        }
 
         return $return;
     }
 
-    public function destroy( Request $request, Response $response )
+    public function destroy( Request $request, Response $response, $args )
     {
-        $id = $args['id'];
+        try{
 
-        $sql = "DELETE FROM students where id = :id";
-        $this->db->prepare($sql);
-        $sth->bindParam(':id', $args['id'], \PDO::PARAM_INT);
-        $sth->execute();
+            $sql = "DELETE FROM students where id = :id";
+            $statement = $this->container->db->prepare($sql);
+            $statement->bindParam(':id', $args['id'], \PDO::PARAM_INT);
+            $statement->execute();
 
-        $return = $response->withJson(['msg' => $sth->rowCount() . " registro(s) deletado(s) com sucesso."], 200)
-        ->withHeader('Content-type', 'application/json');
+            $return = $response->withJson("Operação realizada com sucesso.", 200)
+                ->withHeader('Content-type', 'application/json');
+
+        }catch (\Exception $e){
+
+            $return = $response->withJson("Operação não realizada.", 400)
+                ->withHeader('Content-type', 'application/json');
+        }
 
         return $return;
 
--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 $app->get('/', \Student::class . ':index');
+$app->post('/', \Student::class . ':create');
 $app->get('/{id:[0-9]+}', \Student::class . ':show');
 $app->put('/{id:[0-9]+}', \Student::class . ':update');
-?>
+$app->delete('/{id:[0-9]+}', \Student::class . ':destroy');
